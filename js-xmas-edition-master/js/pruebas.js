ec92ef413c444268f0ba79bcecc6c006f5a30268
nahuel-fed <fgonz1998@gmail.com>
2019-11-26 14:06:37 -0300

    cambia las funciones a pruebas
    necesito escribir las pruebas para saber si funciona bien el código que escribo

--- js-xmas-edition-master/js/pruebas.js
+++ js-xmas-edition-master/js/pruebas.js
@@ -13,3 +13,44 @@
 // }
 
 // probarValidarNombre();
+
+
+$form= document.querySelector("#carta-a-santa")
+
+let provincia= $form.ciudad.value
+let regalo= $form.["descripcion-regalo"].value  
+console.log (provincia)
+console.log(regalo)
+console.assert (provincia === "Chaco."|| "Bs. As.")
+
+
+
+
+
+
+
+
+// function validarProvincia (provincia){
+//      === "Bs. As."
+//     ||validarciudad(provincia) === "Chaco"
+//     ||validarciudad(provincia) === "Chubut"
+//     ||validarciudad(provincia) === "Corrientes"
+//     ||validarciudad(provincia) === "Entre Rios"
+//     ||validarciudad(provincia) === "Formosa"
+//     ||validarciudad(provincia) === "Jujuy"
+//     ||validarciudad(provincia) === "La Pampa"
+//     ||validarciudad(provincia) === "La Rioja"
+//     ||validarciudad(provincia) === "Mendoza"
+//     ||validarciudad(provincia) === "Misiones"
+//     ||validarciudad(provincia) === "Neuquen"
+//     ||validarciudad(provincia) === "Rio Negro"
+//     ||validarciudad(provincia) === "Salta"
+//     ||validarciudad(provincia) === "San Juan"
+//     ||validarciudad(provincia) === "San Luis"
+//     ||validarciudad(provincia) === "Santa Cruz"
+//     ||validarciudad(provincia) === "Santa Fe"
+//     ||validarciudad(provincia) === "Sgo. del Ester"
+//     ||validarciudad(provincia) === "Tierra del Fuego"
+//     ||validarciudad(provincia) === "Tucuman", 
+//     "La selección debe ser una provincia")
+// }
