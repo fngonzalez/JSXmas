02ab5334fa3cd96f304c1839886bdebc64f3c59c
nahuel-fed <fgonz1998@gmail.com>
2019-11-27 16:31:57 -0300

    Pone las cosas en su lugar y borra cosas
    Reordena el código y borré cosas que no iban

--- js-xmas-edition-master/tarea/tarea-clase-7.js
+++ js-xmas-edition-master/tarea/tarea-clase-7.js
@@ -5,3 +5,18 @@
 * Adicional: Escribir pruebas para las funciones de tareas anteriores.
 *
 * */
+
+$form = document.querySelector("#carta-a-santa")
+
+let provincia = $form.ciudad.value
+let regalo = $form["descripcion-regalo"].value
+
+
+function validarRegalo(regalo) {
+    if (regalo.length < 0) {
+        return "Este campo debe tener al menos 1 caracter"
+    } if (regalo.length > 250) {
+        return "Este campo debe tener menos de 250 caracteres"
+    }
+    return ""
+}
