a18732a49ffc3a9c30f5e737672a863e336fa84f
nahuel-fed <fgonz1998@gmail.com>
2020-03-12 15:46:44 -0300

    cambia un texto de validacion
    no me gustaba el anterior

--- js-xmas-edition-master/js/pruebas.js
+++ js-xmas-edition-master/js/pruebas.js
@@ -47,19 +47,19 @@ probarValidarProv()
 
 
 function probarValidarRegalo() {
-    mensaje1= "aca hay 22 caracteres aca hay 22 caracteres aca hay 22 caracteres aca hay 22 caracteres aca hay 22 caracteres aca hay 22 caracteres aca hay 22 caracteres aca hay 22 caracteres aca hay 22 caracteres aca hay 22 caracteres aca hay 22 caracteres aca hay 22 caracteres"
-    mensaje2=""
-    mensaje3="Deseo un buen café por las mañanas"
-    retorna1="Este campo debe tener menos de 250 caracteres"
-    retorna2="Este campo debe contener al menos más de 2 caracteres"
-    retorna3=""
-    error1="La prueba de validarRegalo, no validó que haya menos de 250 caracteres"
-    error2="La prueba de validarRegalo no probó que se hayan escrito más de dos caracteres"
-    error3="La prueba de validarRegalo no probó que se haya escrito bien el regalo"
+    mensaje1 = "Este es un texto en el que necesariamente tienen que existir 250 caracteres. Esto es para conocer si es que el contador de letras de mi programa (el cual es la septima tarea del curso de programación para principiantes) está funcionando de una manera adecuada"
+    mensaje2 = ""
+    mensaje3 = "Deseo un buen café por las mañanas"
+    retorna1 = "Este campo debe tener menos de 250 caracteres"
+    retorna2 = "Este campo debe contener al menos más de 2 caracteres"
+    retorna3 = ""
+    error1 = "La prueba de validarRegalo, no validó que haya menos de 250 caracteres"
+    error2 = "La prueba de validarRegalo no probó que se hayan escrito más de dos caracteres"
+    error3 = "La prueba de validarRegalo no probó que se haya escrito bien el regalo"
 
     pruebaValidacion(validarRegalo, mensaje1, retorna1, error1)
     pruebaValidacion(validarRegalo, mensaje2, retorna2, error2)
     pruebaValidacion(validarRegalo, mensaje3, retorna3, error3)
-    
+
 }
 probarValidarRegalo()
