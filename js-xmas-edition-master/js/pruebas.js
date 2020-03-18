0e8fc95b61367e0a0ebf964c2dae23ac260d38a3
nahuel-fed <fgonz1998@gmail.com>
2020-03-18 15:01:19 -0300

    Arregla un bug diminuto
    borra un } innecesario

--- js-xmas-edition-master/js/pruebas.js
+++ js-xmas-edition-master/js/pruebas.js
@@ -1,13 +1,13 @@
 
 function convertirNodeListAArray(nodeList) {
-    let arrayFinal=[]
+    let arrayFinal = []
     for (let i = 1; i < nodeList.length; i++) {
         arrayFinal.push(nodeList[i].value)
     }
     return arrayFinal
 }
 
-function obtenerForm(){
+function obtenerForm() {
     return document.querySelector("#carta-a-santa")
 }
 
@@ -60,7 +60,7 @@ function probarValidarRegalo() {
     let error1 = "La prueba de validarRegalo, no validó que haya menos de 250 caracteres"
     let error2 = "La prueba de validarRegalo no probó que se hayan escrito más de dos caracteres"
     let error3 = "La prueba de validarRegalo no probó que se haya escrito bien el regalo"
-    
+
     pruebaValidacion(validarRegalo, mensaje1, retorna1, error1)
     pruebaValidacion(validarRegalo, mensaje2, retorna2, error2)
     pruebaValidacion(validarRegalo, mensaje3, retorna3, error3)
@@ -68,12 +68,11 @@ function probarValidarRegalo() {
 }
 probarValidarRegalo()
 
-function probarValidarNombre (){
-    let mensaje=['','Ezequiel', 'Este   texto   tiene  mas   de    50    caracteres ']
-    let retorna= ['Este campo debe contener al menos más de 2 caracteres', '', 'Este campo debe tener menos de 250 caracteres']
-    let error= ["La prueba de validarRegalo no probó que se hayan escrito más de un caracter","La prueba de validarNombre no probó que se haya escrito bien el nombre","La prueba de validarNombre no validó que haya menos de 50 caracteres"]
-    pruebaValidacion(validarProvincia, mensaje[0],retorna[0],error[0])
-    pruebaValidacion(validarProvincia, mensaje[1],retorna[1],error[1])
-    pruebaValidacion(validarProvincia, mensaje[2],retorna[2],error[2])
-    }
+function probarValidarNombre() {
+    let mensaje = ['', 'Ezequiel', 'Este   texto   tiene  mas   de    50    caracteres ']
+    let retorna = ['Este campo debe contener al menos más de 2 caracteres', '', 'Este campo debe tener menos de 250 caracteres']
+    let error = ["La prueba de validarRegalo no probó que se hayan escrito más de un caracter", "La prueba de validarNombre no probó que se haya escrito bien el nombre", "La prueba de validarNombre no validó que haya menos de 50 caracteres"]
+    pruebaValidacion(validarProvincia, mensaje[0], retorna[0], error[0])
+    pruebaValidacion(validarProvincia, mensaje[1], retorna[1], error[1])
+    pruebaValidacion(validarProvincia, mensaje[2], retorna[2], error[2])
 }
